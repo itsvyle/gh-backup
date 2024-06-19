7469ae1f296c380beef8f1b7d499de77eed68310
Greg Mat. <65409906+itsvyle@users.noreply.github.com>
2024-06-19 16:29:48 +0000

    Start adding flags for the command

--- config/config.go
+++ config/config.go
@@ -6,16 +6,18 @@ import (
 )
 
 var (
-	BackupPrivateRepos        = true
-	BackupOtherOwnersRepos    = false
-	ConcurrentRepoDownloads   = 5
-	LocalStoragePath          = "/tmp/ghbackup"
-	ForceRedownload           = false
-	BranchesToCheckForChanges = []string{"master", "main"}
+	BackupPrivateRepos      = true
+	BackupOtherOwnersRepos  = false
+	ConcurrentRepoDownloads = 5
+	LocalStoragePath        = "/tmp/ghbackup"
+	ForceRedownload         = false
 )
+
+var force = flag.Bool("force", false, "Force redownload of all repos")
 
 func init() {
 	flag.Parse()
+	ForceRedownload = *force
 }
 
 func InitConfig() {
